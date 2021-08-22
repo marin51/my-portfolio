cf0015da7a4bef965f67ee1b9818cd0e12d21fdd
marin97 <marinoff3417@gmail.com>
2021-08-22 11:28:08 +0300

    router and about page

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,44 @@
 <template>
     <div id="app">
-      <home></home>
+        <div class="navigation">
+            <div class="wrapper">
+                <b-card title="My portfolio" bg-variant="secondary" class="rounded-0" text-variant="white">
+                    <b-card-text>
+                        Welcome to my site.
+                    </b-card-text>
+                </b-card>
+                <b-nav>
+                    <b-nav-item to="Home" active-class="active">Home</b-nav-item>
+                    <b-nav-item to="About" active-class="active">About</b-nav-item>
+                </b-nav>
+            </div>
+        </div>
+        <router-view></router-view>
     </div>
 </template>
 
 <script>
-import Home from './components/Home.vue'
+    import Home from './views/Home.vue'
 
-export default {
-  components: {
-    Home
-  }
-}
+    export default {
+        components: {
+            Home
+        }
+    }
 </script>
+<style lang="scss">
+    #app {
+        padding: 0 10%;
+
+    }
+
+    .navigation {
+        display: flex;
+        justify-content: center;
+
+        .wrapper {
+            width: 100%;
+            padding-top: 2rem;
+        }
+    }
+</style>
--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,32 @@
 import Vue from 'vue'
-import Router from 'vue-router'
+import Router, {RouteConfig} from 'vue-router'
+import Home from './views/Home'
+import About from './views/About'
 
 Vue.use(Router);
+const routes: Array<RouteConfig> = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/About',
+        name: 'About',
+        component: About
+    }
+];
+const router = new Router({
+    mode: 'history',
+    routes
+});
+router.beforeEach((to, from, next) => {
+    const found = routes.find(route => route.name === to.name);
+    if (found) {
+        next();
+    } else {
+        next({name: 'Home'});
+    }
+});
+export default router
 
-export default new Router({
-    mode: 'history'
-})
